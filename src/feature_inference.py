3d29885f550b5241337fe57669d476c9df55b306
Sema Halitoglu <hali1670@mylaurier.ca>
2025-12-09 18:57:50 -0500

    Create feature_inference.py

---
+++ src/feature_inference.py
@@ -0,0 +1,10 @@
+def infer_features(query):
+    return [
+        "reading ability",
+        "writing ability",
+        "parental education",
+        "gender",
+        "race group",
+        "lunch type",
+        "test preparation course"
+    ]
