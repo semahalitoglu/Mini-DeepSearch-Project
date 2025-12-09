3ee6b3071300ec5883cf2d9a9a217db2384e73b2
Sema Halitoglu <hali1670@mylaurier.ca>
2025-12-09 18:58:54 -0500

    Create column_mapping.py

---
+++ src/column_mapping.py
@@ -0,0 +1,10 @@
+def map_columns():
+    return {
+        "reading ability": "reading score",
+        "writing ability": "writing score",
+        "parental education": "parental level of education",
+        "gender": "gender",
+        "race group": "race/ethnicity",
+        "lunch type": "lunch",
+        "test preparation course": "test preparation course"
+    }
